576d7ecb3e1b6c94667bb9908599f9a151315ad8
GERVASIOAGF <gervasioagf@users.noreply.github.com>
2020-06-19 23:50:38 -0300

    slick fotos 19.06.2020

--- _js/script.js
+++ _js/script.js
@@ -1,13 +1,38 @@
 $(function () {
 
+    function atualizar_informacoes() {
+        $("#descricao-jogador").text( $(".slick-center").data("name"));
+        $("#posicao-jogador").text( $(".slick-center").data("price"));
+    }
+
+    $(".galeria-fotos").on('init', function() {
+        atualizar_informacoes();
+    
+    });
+
     $(".galeria-fotos").slick({
        infinite: true,
        slidesToShow: 3,
        slidesToScroll: 1,
        centerMode: true,
        prevArrow: $("#seta-esquerda"),
-       nextArrow: $("#seta-direita")
+       nextArrow: $("#seta-direita"),
+       responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $(".galeria-fotos").on('afterChange', function() {
+        atualizar_informacoes();
+    
     });
 
 
-})
+});
+
+
